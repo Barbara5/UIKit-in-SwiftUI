55e18a75900830e9b5e419af97e5e560700592b4
Barbara <barbarabeatrice95@gmail.com>
2022-04-06 00:18:03 +0200

    changed url

--- UIKit in SwiftUI/UIKit in SwiftUI/ContentView.swift
+++ UIKit in SwiftUI/UIKit in SwiftUI/ContentView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         
-     WebView(url: URL(string: "https://www.notion.so/appledeveloperacademyunina/B-Artifact-1-a4f678182b614c0c928abb1361f3ca61")!)
+//        Call the WebView and add the url of the page you want to show
+     WebView(url: URL(string: "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2016/03/giphy-2.gif")!)
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
